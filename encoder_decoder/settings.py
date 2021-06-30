708afa0ee6cdb6ec0ec10e63a402e247274fee5c
sweall1 <bardziszewski@wp.pl>
2021-06-30 16:46:18 +0200

    heroku

--- encoder_decoder/settings.py
+++ encoder_decoder/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import django_heroku
+from pathlib import Path
 from .secret_settings import *
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,14 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'encoder_decoder/static')
+    os.path.join(BASE_DIR, 'encoder_decoder/static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
