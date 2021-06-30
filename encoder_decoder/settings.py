186a1ab945cbb94e2f3c18f5f1154feb86a606c9
sweall1 <bardziszewski@wp.pl>
2021-06-30 17:12:40 +0200

    heroku

--- encoder_decoder/settings.py
+++ encoder_decoder/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'encoder_decoder/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
