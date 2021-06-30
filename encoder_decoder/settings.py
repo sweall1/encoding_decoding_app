85363ac75e307c55774624c138bda2ae9e9c0cfd
sweall1 <bardziszewski@wp.pl>
2021-06-30 20:18:38 +0200

    heroku

--- encoder_decoder/settings.py
+++ encoder_decoder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b+^1-tiqi#lywgr#++z(2v6stamt8$o#z0)!+b4kg-2(y&s2p2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['encoder_decoder.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
